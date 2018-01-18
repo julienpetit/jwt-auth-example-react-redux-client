fa3b45f6990f4c57b11a7d4d1200e693ebe9e361
Julien PETIT <contact@julienpetit.fr>
2018-01-18 13:59:25 +0100

    Configure redux saga and implement login call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import auth from './modules/auth';
 import './App.css';
+import logo from './logo.svg';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+        };
+
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+
+    handleOnClick() {
+        const { username, password } = this.state;
+        const { dispatch } = this.props;
+
+        dispatch(auth.actions.loginRequest({
+            username,
+            password,
+        }));
+    }
+
+    render() {
+
+        const { username, password } = this.state;
+        const { isAuthenticated } = this.props;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+
+                {!isAuthenticated && (
+                    <form>
+                        <input type="text" value={username} name="username" onChange={(e) => this.setState({ username: e.target.value })} />
+                        <input type="password" value={password} name="password" onChange={(e) => this.setState({ password: e.target.value })} />
+                        <button type="button" onClick={this.handleOnClick}>
+                            Login
+                        </button>
+                    </form>
+                )}
+
+                {isAuthenticated && (
+                    <h2>Bienvenue</h2>
+                )}
+
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = ({ auth }) => ({
+    isAuthenticated: auth.token !== null,
+});
+
+const connected = connect(mapStateToProps, dispatch => ({ dispatch }))(App);
+
+export default connected;
--- src/modules/auth/reducer.js
+++ src/modules/auth/reducer.js
@@ -1,9 +1,17 @@
-const INITIAL_STATE = {
+import * as t from './actionTypes';
 
+const INITIAL_STATE = {
+    token: null,
 };
 
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
+
+        case t.LOGIN_SUCCESS:
+            return {
+                ...state,
+                ...action.payload,
+            };
 
         default:
             return state;
