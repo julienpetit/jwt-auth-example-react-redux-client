e6ea377388b27bfb52124cb0cfb8fdf939c171c2
Julien PETIT <contact@julienpetit.fr>
2018-01-18 15:48:00 +0100

    Store token in localStorage

--- src/modules/auth/constants.js
+++ src/modules/auth/constants.js
@@ -1,1 +1,2 @@
 export const NAME = 'auth';
+export const LS_TOKEN = 'token';
--- src/modules/auth/sagas.js
+++ src/modules/auth/sagas.js
@@ -2,14 +2,16 @@ import { takeEvery, call, put } from 'redux-saga/effects';
 import authApi from './api';
 import { loginSuccess, loginError } from './actions';
 import * as t from './actionTypes';
+import * as constants from './constants';
 
 export function* authorize(action) {
     try {
         const loginResult = yield call(authApi.login, action);
         yield put(loginSuccess(loginResult));
+
+        localStorage.setItem(constants.LS_TOKEN, JSON.stringify(loginResult.token));
     }
     catch (error) {
-        console.log('error');
         yield put(loginError(error));
     }
 }
